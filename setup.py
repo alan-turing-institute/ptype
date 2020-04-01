cd8809275c2a69925065db463dc9a3fd9ca8461c
Roly Perera <rperera@turing.ac.uk>
2020-04-01 12:55:26 +0100

    Minimal .gitignore.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+   name='ptype',
+   version='1.0',
+   description='Probabilistic type inference',
+   author='Taha Ceritli, Christopher K. I. Williams, James Geddes',
+   author_email='t.y.ceritli@sms.ed.ac.uk, ckiw@inf.ed.ac.uk, jgeddes@turing.ac.uk',
+   url='https://github.com/tahaceritli/ptype-dmkd',
+   packages=[]
+)
