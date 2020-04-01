74cbfef0f1f0eb420f469db0d3ed33e9777fd903
Roly Perera <rperera@turing.ac.uk>
2020-04-01 18:14:44 +0100

    Run in virtualenv; install nbformat.

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # Build Python package. Make this more repeatable/platform-independent as time goes on.
 
-python -m pip install --upgrade pip greenery setuptools wheel
+# run in virtualenv to avoid unwanted interactions with e.g. MacPorts which installs with sudo
+python -m virtualenv venv
+source ./venv/bin/activate
+python -m pip install --upgrade pip greenery nbformat setuptools wheel
 python ../setup.py sdist
+deactivate
+rm -rf venv
