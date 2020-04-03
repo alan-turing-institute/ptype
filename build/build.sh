b966264e787c91109347cc6716317fa50e616a87
Roly Perera <rperera@turing.ac.uk>
2020-04-03 15:14:58 +0100

    Use python3 in build script.

--- build/build.sh
+++ build/build.sh
@@ -2,16 +2,19 @@
 # Build Python package. Make this more repeatable/platform-independent as time goes on.
 
 # run in virtualenv to avoid unwanted interactions with e.g. MacPorts which installs with sudo
-python -m virtualenv venv
+python3 -m virtualenv venv
 source ./venv/bin/activate
 
 # build source distribution
-python -m pip install --upgrade pip greenery jupyter_client ipykernel nbconvert nbformat setuptools wheel
-python ../setup.py sdist
+python3 -m pip install --upgrade pip greenery jupyter_client ipykernel nbconvert nbformat setuptools wheel
+python3 ../setup.py sdist
+
+# Presumably a better way to do this, although PyCharm does something similar.
+python3 -c "import sys; sys.path.extend(['src', 'test'])"
 
 # test
 pushd ..
-python tests/test_ptype.py
+python3 tests/test_ptype.py
 popd
 # python notebook_runner.py "../notebooks/demo.ipynb"
 
