ff9975333a46924c876347e930a65f93b1f242a2
Roly Perera <rperera@turing.ac.uk>
2020-04-03 17:55:08 +0100

    GitHub workflow passes location of Python 3.8.

--- build/build.sh
+++ build/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# Build Python package. Make this more repeatable/platform-independent as time goes on.
+# Build Python package.
 
 # doesn't seem to work if I run in a virtualenv (can't find 'src' module)
-pyexe=/usr/local/bin/python3.8
+# default to local Python 3.8 installation; use argument when building in GitHub runner.
+pyexe=${1:-/usr/local/bin/python3.8}
 
 # build source distribution
 $pyexe ../setup.py sdist || exit 1
@@ -12,7 +13,7 @@ pushd ..
 
 $pyexe tests/test_ptype.py || exit 1
 # for now discard any result disparities; will check these later
-git checkout tests/column_type_counts.cvs
+git checkout tests/column_type_counts.csv
 git checkout tests/column_type_predictions.json
 
 popd ... || exit
