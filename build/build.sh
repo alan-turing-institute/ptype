439bd11b27da56502d37f3a8236a242c2182d592
Roly Perera <rperera@turing.ac.uk>
2020-04-03 16:28:20 +0100

    Try with export PYTHONPATH instead.

--- build/build.sh
+++ build/build.sh
@@ -6,15 +6,14 @@ python3 -m virtualenv venv
 source ./venv/bin/activate
 
 # build source distribution
-python3 -m pip install --upgrade pip greenery jupyter_client ipykernel nbconvert nbformat setuptools wheel
-python3 ../setup.py sdist
+python ../setup.py sdist
 
-# Presumably a better way to do this, although PyCharm does something similar.
-python3 -c "import sys; sys.path.extend(['src', 'test'])"
+# more idiomatic way to achieve this?
+export PYTHONPATH="src;test"
 
 # test
 pushd ..
-python3 tests/test_ptype.py
+python tests/test_ptype.py
 popd
 # python notebook_runner.py "../notebooks/demo.ipynb"
 
