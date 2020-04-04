0ec715444492198626bab72dd1b2d55a245f9397
Roly Perera <rperera@turing.ac.uk>
2020-04-04 17:21:02 +0100

    Think I can run in virtualenv.

--- build/build.sh
+++ build/build.sh
@@ -5,9 +5,11 @@ set -x
 # doesn't seem to work if I run in a virtualenv (can't find 'src' module)
 # default to local Python 3.8 installation; use argument when building in GitHub runner.
 pyexe=${1:-/usr/local/bin/python3.8}
+pyexe -m virtualenv venv
+source venv/bin/activate
 
 # build source distribution
-$pyexe ../setup.py sdist || exit 1
+python ../setup.py sdist || exit 1
 
 # test
 pushd ..
@@ -15,12 +17,14 @@ pushd ..
 # this seems to be included by default, but not in the GitHub runner
 export PYTHONPATH=.
 
-$pyexe tests/test_ptype.py || exit 1
+python tests/test_ptype.py || exit 1
 # show disparities, then discard; will check these later
 git diff tests
 git checkout tests/column_type_counts.csv
 git checkout tests/column_type_predictions.json
 
 popd ... || exit
+deactivate
+rm -rf venv
 
 # python notebook_runner.py "../notebooks/demo.ipynb"
