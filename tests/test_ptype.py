5e8470e837d88f476baabffd0cc218790395dd37
Taha Ceritli <t.y.ceritli@sms.ed.ac.uk>
2020-04-09 13:03:22 +0100

    handles BOM char and pretty printing for JSON files

--- tests/test_ptype.py
+++ tests/test_ptype.py
@@ -1,3 +1,11 @@
+def read_data(_data_path, dataset_name):
+    # wrong encoding leads to additional characters in the dataframe columns
+    if dataset_name in ['mass_6.csv', ]:
+        encoding = 'ISO-8859-1'
+    else:
+        encoding = 'utf-8'
+    return csv.csv2df(_data_path + dataset_name, encoding=encoding, dtype=str, skipinitialspace=True)
+
 def get_predictions(_data_path):
     dataset_names = get_datanames()
 
@@ -10,7 +18,8 @@ def get_predictions(_data_path):
     # run ptype on each dataset
     type_predictions = {}
     for dataset_name in dataset_names:
-        df = csv.csv2df(_data_path + dataset_name, encoding='ISO-8859-1', dtype=str, skipinitialspace=True)
+
+        df = read_data(_data_path, dataset_name)
         ptype.run_inference(_data_frame=df)
 
         # store types
@@ -26,10 +35,12 @@ def main(_data_path='data/',
     annotations = json.load(open(_annotations_path))
 
     type_predictions = get_predictions(_data_path)
-    json.dump(type_predictions, open(_predictions_path, 'w'))
+
+    # does not write optional BOM char and perfors pretty printing for json file
+    with open(_predictions_path, 'w', encoding='utf-8-sig') as write_file:
+            json.dump(type_predictions, write_file, indent=2, sort_keys=True, ensure_ascii=False)
 
     evaluate_predictions(_data_path, annotations, type_predictions)
-
 
 if __name__ == "__main__":
     from src.utils import get_datanames, evaluate_predictions
