c7e86532b77e691a6052bb0847c8062122536d3f
Roly Perera <rperera@turing.ac.uk>
2020-04-28 19:07:31 +0100

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
    name='ptype',
-   version='0.0.1',
+   version='0.0.2',
    description='Probabilistic type inference',
    long_description=long_description,
    long_description_content_type="text/markdown",
