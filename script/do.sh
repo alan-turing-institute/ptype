4ffc5dc4d8665385c53d35bd69425a2acec119ce
Roly Perera <rperera@turing.ac.uk>
2020-04-28 19:43:58 +0100

    All getting a bit complicated. do-local script which runs do with local Python 3.8 installation; do now requires a Python binary, and then passes all remaining arguments to the target script.

--- script/do.sh
+++ script/do.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 # Run another script inside a Python virtualenv.
 # $1: other script to run
-# $2: Python binary to use for virtualenv
 set -u -o xtrace
 
-# default to local Python 3.8 installation; use argument when building in GitHub runner
-pyexe=${2:-/usr/local/bin/python3.8}
+pyexe=$1
 $pyexe -m pip install virtualenv
 $pyexe -m virtualenv venv || exit 1
 source venv/bin/activate
+shift
 # shellcheck source=/dev/null.
-source "$1"
+source "$@"
 deactivate || exit 1
 rm -rf venv
 
