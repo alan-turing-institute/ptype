bb72343fa90222602a5aae46811c5056b423275c
Roly Perera <rperera@turing.ac.uk>
2020-04-28 19:20:31 +0100

    Run in package root.

--- script/build-publish.sh
+++ script/build-publish.sh
@@ -1,3 +1,7 @@
-bump-patch-version.sh
-build.sh
-publish.sh "$1"
+#!/bin/bash
+# Build and publish a new patch version. Run from package root.
+# $1: PyPI password
+
+source script/bump-patch-version.sh
+source script/build.sh
+source script/publish.sh "$1"
