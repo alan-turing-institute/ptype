7d07be902143cfa138c1f9270582144837417543
Roly Perera <rperera@turing.ac.uk>
2020-04-29 13:23:21 +0100

    Parameterise publish.sh on PyPI repo to use.

--- script/do-build-test-publish.sh
+++ script/do-build-test-publish.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -e
 # $1: Python binary to use for virtualenv
 # $2: PyPI password
+# $3: PyPI repository to use (pypi or testpypi)
 
 script/do.sh "$1" script/bump-patch-version.sh
 source script/do-build-test.sh "$1"
-script/do.sh "$1" script/publish.sh "$2"
+script/do.sh "$1" script/publish.sh "$2" "$3"
 git commit -a -m "Bump version number."
 git push
 git push --tags
--- script/publish.sh
+++ script/publish.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
 # Publish contents of dist folder to PyPI.
 # $1: PyPI password
+# $2: PyPI repository to use (pypi or testpypi)
 
 python -m pip install --upgrade twine==3.1.1
-python -m twine upload -u __token__ -p "$1" -r testpypi dist/*
+python -m twine upload -u __token__ -p "$1" -r "$2" dist/*
 echo Uploaded to PyPI.
