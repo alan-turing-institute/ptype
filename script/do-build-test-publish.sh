e55b1e18ee2c34ab2318401b4900a4d1140457aa
Roly Perera <rperera@turing.ac.uk>
2020-04-29 12:33:54 +0100

    Bump version number.

--- script/do-build-test-publish.sh
+++ script/do-build-test-publish.sh
@@ -5,3 +5,6 @@
 script/do.sh "$1" script/bump-patch-version.sh
 source script/do-build-test.sh "$1"
 script/do.sh "$1" script/publish.sh "$2"
+git commit -a -m "Bump version number."
+git push
+git push --tags
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
    name='ptype',
-   version='0.1.7',
+   version='0.1.8',
    description='Probabilistic type inference',
    long_description=long_description,
    long_description_content_type="text/markdown",
