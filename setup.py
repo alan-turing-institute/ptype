a010809f334c5be5e6552448f5c3adb9b5f10001
runner <runner@Mac-1563.local>
2020-04-29 12:38:36 +0000

    Bump version number.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
    name='ptype',
-   version='0.1.11',
+   version='0.1.12',
    description='Probabilistic type inference',
    long_description=long_description,
    long_description_content_type="text/markdown",
