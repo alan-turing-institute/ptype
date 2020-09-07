901e57e92ae2d8aea20a87fb89888c405cb4f079
Roly Perera <rperera@turing.ac.uk>
2020-09-07 16:51:01 +0100

    Consolidating test infrastructure.

--- tests/test_ptype.py
+++ tests/test_ptype.py
@@ -31,10 +31,12 @@ def as_anomaly(ptype):
         series.map(lambda v: v if v in ptype.cols[series.name].get_anomaly_predictions() else pd.NA)
 
 
-def check_predictions(dataset_name):
+def get_predictions(dataset_name, infer_canonical_types=False):
     data_folder = "data/"
-    expected_folder = "tests/expected"
+    model_folder = "models/"
     df = read_data(data_folder, dataset_name)
+    normalizer = joblib.load(model_folder + "robust_scaler.pkl")
+    clf = joblib.load(model_folder + "LR.sav")
 
     ptype = Ptype(_types={
         1: "integer",
@@ -48,17 +50,12 @@ def check_predictions(dataset_name):
     })
     ptype.run_inference(_data_frame=df)
 
-    expected_file = expected_folder + "/" + os.path.splitext(dataset_name)[0] + ".json"
-    with open(expected_file, 'r', encoding='utf-8-sig') as read_file:
-        expected = json.load(read_file)
-
-    type_predictions = {col_name: col.predicted_type for col_name, col in ptype.cols.items()}
-
-    if not (type_predictions == expected):
-        # prettyprint new JSON, omiting optional BOM char
-        with open(expected_file + '.new', 'w', encoding='utf-8-sig') as write_file:
-            json.dump(type_predictions, write_file, indent=2, sort_keys=True, ensure_ascii=False)
-        raise Exception(f'{expected_file} comparison failed.')
+    if infer_canonical_types:
+        for col_name in ptype.cols:
+            # normalize the features as done before, then reclassify the column
+            features = ptype.features[col_name]
+            features[[7, 8]] = normalizer.transform(features[[7, 8]].reshape(1, -1))[0]
+            ptype.cols[col_name].predicted_type = clf.predict(features.reshape(1, -1))[0]
 
     df_missing = df.apply(as_missing(ptype), axis=0)
     df_anomaly = df.apply(as_anomaly(ptype), axis=0)
@@ -69,50 +66,20 @@ def check_predictions(dataset_name):
     print("Anomalies:\n", df_anomaly)
     print("Normal:\n", df_normal)
 
-    return type_predictions
+    return {col_name: col.predicted_type for col_name, col in ptype.cols.items()}
 
 
-def get_canonical_predictions(_data_path, _model_folder):
-    dataset_names = get_datasets()
+def check_predictions(type_predictions, dataset_name):
+    expected_folder = "tests/expected"
+    expected_file = expected_folder + "/" + os.path.splitext(dataset_name)[0] + ".json"
+    with open(expected_file, 'r', encoding='utf-8-sig') as read_file:
+        expected = json.load(read_file)
 
-    # create ptype
-    types = {
-        1: "integer",
-        2: "string",
-        3: "float",
-        4: "boolean",
-        5: "date-iso-8601",
-        6: "date-eu",
-        7: "date-non-std-subtype",
-        8: "date-non-std",
-    }
-    ptype = Ptype(_types=types)
-
-    normalizer = joblib.load(_model_folder + "robust_scaler.pkl")
-    clf = joblib.load(_model_folder + "LR.sav")
-
-    # run ptype on each dataset
-    type_predictions = {}
-    for dataset_name in dataset_names:
-
-        df = read_data(_data_path, dataset_name)
-        ptype.run_inference(_data_frame=df)
-
-        # infer canonical types
-        for col_name in ptype.cols:
-            # get features
-            features = ptype.features[col_name]
-
-            # normalize the features as done before
-            features[[7, 8]] = normalizer.transform(features[[7, 8]].reshape(1, -1))[0]
-
-            # classify the column
-            ptype.cols[col_name].predicted_type = clf.predict(features.reshape(1, -1))[0]
-
-        # store types
-        type_predictions[dataset_name] = {col_name: ptype.cols[col_name].predicted_type for col_name in ptype.cols}
-
-    return type_predictions
+    if not (type_predictions == expected):
+        # prettyprint new JSON, omiting optional BOM char
+        with open(expected_file + '.new', 'w', encoding='utf-8-sig') as write_file:
+            json.dump(type_predictions, write_file, indent=2, sort_keys=True, ensure_ascii=False)
+        raise Exception(f'{expected_file} comparison failed.')
 
 
 def notebook_tests():
@@ -122,12 +89,12 @@ def notebook_tests():
 
 
 def main():
-    annotations_file = "annotations/annotations.json"
-    annotations = json.load(open(annotations_file))
+    annotations = json.load(open("annotations/annotations.json"))
 
     type_predictions = {}
     for dataset_name in get_datasets():
-        type_predictions[dataset_name] = check_predictions(dataset_name)
+        type_predictions[dataset_name] = get_predictions(dataset_name)
+        check_predictions(type_predictions[dataset_name], dataset_name)
 
     evaluate_predictions(annotations, type_predictions)
 
