4402f851b3d2facb02769714df75011948cb5963
Roly Perera <rperera@turing.ac.uk>
2020-09-30 13:38:33 +0100

    Need to get a stream with output of 'git diff'.

--- tests/test_ptype.py
+++ tests/test_ptype.py
@@ -152,7 +152,9 @@ def check_expected(actual, filename):
     actual_str = jsonpickle.encode(actual, indent=2)
     if expected_str != actual_str:  # deep comparison
         print("Result of 'git diff':")
-        os.system("ls -l")
+        stream = os.popen("ls -l")
+        output = stream.read()
+        print(output)
         os.system(f"git diff {filename_}")
         with open(filename_, "w") as file:
             file.write(actual_str)
