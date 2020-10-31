bc48dfbe9d8a1d78eb5c7a83ffa3748398f4954c
Roly Perera <rperera@turing.ac.uk>
2020-10-31 13:41:15 +0000

    Reinstate pytest_collect_start solution.

--- conftest.py
+++ conftest.py
@@ -1,4 +1,7 @@
 def pytest_collectstart(collector):
-    pass
-#    if hasattr(collector, 'skip_compare'):
-#        collector.skip_compare += 'text/html', 'application/javascript', 'stderr',
+    # We need to ignore UserWarning: Trying to unpickle estimator when loading a pickle from an earlier
+    # version of sklearn. The best solution would be to use a regex in nbval_sanitize.cfg to replace the
+    # warnings by a constant string, but I haven't been able to reproduce them outside the test environment.
+    # This is a little coarse-grained, since it will disregard all stderr output.
+    if hasattr(collector, 'skip_compare'):
+        collector.skip_compare += 'text/html', 'application/javascript', 'stderr',
