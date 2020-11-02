31001c84b124126f4ad482e1594c1ef766d1cb28
Roly Perera <rperera@turing.ac.uk>
2020-11-02 12:09:58 +0000

    With separate source/build dir can no longer fix index.rst...

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,6 +32,8 @@ extensions = [
     'sphinx.ext.autodoc'
 ]
 
+master_doc = 'source/index'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
