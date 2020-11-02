e274d22a4f9ccfeeb17d3c8d98de47bf484bfca2
Roly Perera <rperera@turing.ac.uk>
2020-11-02 12:35:29 +0000

    This sucks.

--- docs/conf.py
+++ docs/conf.py
@@ -10,10 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
+# import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
-# sys.path.insert(0, '../')
+# sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, '../')
 
 
 # -- Project information -----------------------------------------------------
@@ -46,7 +46,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
+# html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- docs/index.rst
+++ docs/index.rst
@@ -3,8 +3,9 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to ptype's documentation!
-=================================
+===================================
+ptype: probabilistic type inference
+===================================
 
 .. toctree::
    :maxdepth: 2
