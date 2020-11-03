2385fc53649fe893aeb563d63aae50ab32c41193
Roly Perera <rperera@turing.ac.uk>
2020-11-03 09:18:15 +0000

    Still fails. Try twine check.

--- script/publish.sh
+++ script/publish.sh
@@ -4,5 +4,6 @@
 # $2: PyPI repository to use (pypi or testpypi)
 
 python -m pip install --upgrade twine==3.2.0
+python -m twine check dist/*
 python -m twine upload -u __token__ -p "$1" -r "$2" dist/*
 echo Uploaded to PyPI.
