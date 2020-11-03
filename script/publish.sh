17a72c153ba55dea4f034650ec113412329d7326
Roly Perera <rperera@turing.ac.uk>
2020-11-03 08:56:27 +0000

    Try bumping Twine to 3.2.0.

--- script/publish.sh
+++ script/publish.sh
@@ -3,6 +3,6 @@
 # $1: PyPI password
 # $2: PyPI repository to use (pypi or testpypi)
 
-python -m pip install --upgrade twine==3.1.1
+python -m pip install --upgrade twine==3.2.0
 python -m twine upload -u __token__ -p "$1" -r "$2" dist/*
 echo Uploaded to PyPI.
